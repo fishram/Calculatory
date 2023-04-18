a87f7bcee3acb6c0365196f66b9ceab946ea49d4
Fisher Ramsey <fishram@unc.edu>
2023-04-18 15:55:44 -0400

    animations added

--- Calculatory/ContentView.swift
+++ Calculatory/ContentView.swift
@@ -7,7 +7,9 @@
 
 import SwiftUI
 
+// An enumeration representing the various calculator buttons
 enum CalcButton: String {
+    // Number buttons
     case one = "1"
     case two = "2"
     case three = "3"
@@ -18,6 +20,8 @@ enum CalcButton: String {
     case eight = "8"
     case nine = "9"
     case zero = "0"
+    
+    // Function buttons
     case ac = "AC"
     case negative = "+/-"
     case percent = "%"
@@ -28,11 +32,11 @@ enum CalcButton: String {
     case equals = "="
     case decimal = "."
     
+    // Determines the button's color based on its type
     var buttonColor: Color {
         switch self {
         case .plus, .minus, .multiply, .divide, .equals:
             return .orange
-            
         case .ac, .negative, .percent:
             return Color(.lightGray)
         default:
@@ -42,7 +46,9 @@ enum CalcButton: String {
 }
 
 struct ContentView: View {
+    @State private var tappedButton: CalcButton? = nil
     
+    // A 2D array representing the layout of the calculator buttons
     let buttonGrid: [[CalcButton]] = [
         [.ac, .negative, .percent, .divide],
         [.seven, .eight, .nine, .multiply],
@@ -53,47 +59,67 @@ struct ContentView: View {
     
     var body: some View {
         ZStack {
+            // Set the background color to black
             Color.black.ignoresSafeArea()
+            
             VStack {
-               
+                // Display the current input/result
+                Spacer()
                 HStack {
+
                     Spacer()
                     Text("0")
                         .font(.system(size: 80))
-                    .foregroundColor(.white)
+                        .foregroundColor(.white)
                 }
-                ForEach(buttonGrid, id: \.self) { row in
-                    HStack (spacing: 12){
-                        ForEach(row, id: \.self) { item in
-                            Button(action: {
-                                
-                            }, label: {
-                                Text(item.rawValue)
-                                    .font(.system(size: 40))
-                                    .frame(
-                                        width: self.buttonWidth(item: item), height: self.buttonHeight()
-                                    )
-                                    .background(item.buttonColor)
-                                    .foregroundColor(.white)
-                                    .cornerRadius(self.buttonWidth(item: item)/2)
-                                    
-                            })
-                        }
-                    }
-                    .padding(.bottom, 3 )
-                }
+                .padding(.top, 10)
                 
+                // Loop through the button grid and create the buttons
+                ForEach(buttonGrid, id: \.self) { row in
+                            HStack (spacing: 12){
+                                ForEach(row, id: \.self) { item in
+                                    Button(action: {
+                                        tappedButton = item
+                                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                                            tappedButton = nil
+                                        }
+                                        // Button action here
+                                    }, label: {
+                                        ZStack(alignment: item == .zero ? .leading : .center) {
+                                            RoundedRectangle(cornerRadius: self.buttonWidth(item: item)/2)
+                                                .fill(item.buttonColor)
+                                            
+                                            Text(item.rawValue)
+                                                .font(.system(size: 40))
+                                                .padding(item == .zero ? EdgeInsets(top: 0, leading: 30, bottom: 0, trailing: 0) : EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                                            
+                                            if tappedButton == item {
+                                                RoundedRectangle(cornerRadius: self.buttonWidth(item: item)/2)
+                                                    .fill(Color.white.opacity(0.2))
+                                            }
+                                        }
+                                    })
+                                    .frame(width: self.buttonWidth(item: item), height: self.buttonHeight())
+                                    .foregroundColor(.white)
+                                    .animation(.easeInOut(duration: 0.1), value: tappedButton == item)
+                                }
+                            }
+                            .padding(.bottom, 3 )
+                        }
             }
             .padding()
         }
     }
     
+    // Calculate the button width depeding on if it's a zero or not
     func buttonWidth(item: CalcButton) -> CGFloat {
         if item == .zero{
             return ((UIScreen.main.bounds.width - (3*12))/4) * 2
         }
         return (UIScreen.main.bounds.width - (5*12)) / 4
     }
+    
+    // Calculate the button height
     func buttonHeight() -> CGFloat {
         return (UIScreen.main.bounds.width - (5*12)) / 4
     }
